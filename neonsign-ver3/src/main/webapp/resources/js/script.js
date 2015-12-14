1aa7caad1aede3e04030a77306c7d4bc7a09ed54
eoguq4384 <eoguq4384@gmail.com>
2015-12-14 16:58:38 +0900

    2015 12 14 고대협  추가내용 : 	- completeMainArticleView.jsp : 카드박스 삭제 및 article 추가 	- main.jsp : 태그소트 아래 추가 	- BoardController.java : selectCompleteMainArticle 변수 수정 	- script.js : 태그, 완결게시물 수정

--- neonsign-ver3/src/main/webapp/resources/js/script.js
+++ neonsign-ver3/src/main/webapp/resources/js/script.js
@@ -103,18 +103,6 @@ $(document).ready(function(){ //DOM이 준비되고
 				success:function(data){
 					if(data.completeMainArticleArrayList.length!=0){
 					for(var i=0; i<data.completeMainArticleArrayList.length; i++){
-						//타이틀 길이제한 조건문
-						if(data.completeMainArticleArrayList[i].mainArticleTitle.length>12){
-							mainArticleTitle = data.completeMainArticleArrayList[i].mainArticleTitle.substring(0,12) + " ...";
-						}else{
-							mainArticleTitle = data.completeMainArticleArrayList[i].mainArticleTitle;
-						}
-						//내용 길이제한 조건문
-						if(data.completeMainArticleArrayList[i].mainArticleContent.length>18){
-							mainArticleContent = data.completeMainArticleArrayList[i].mainArticleContent.substring(0,15) + " ...";
-						}else{
-							mainArticleContent = data.completeMainArticleArrayList[i].mainArticleContent;
-						}
 						//잇자버튼을 위한 조건문
 						var mainLikeItHTML="";
 						  if(data.itjaMemberList!=null){
@@ -185,41 +173,45 @@ $(document).ready(function(){ //DOM이 준비되고
               				
 						//추가될 카드 html문
 						infinityScrollTestSource +=
-							'<div class="card-box col-md-4" name="completeCardBox">' 
-							+ '<div class="card card-with-border" data-background="image" style="background-image: url(resources/uploadImg/articleBg/'+data.completeMainArticleArrayList[i].mainArticleImgVO.mainArticleImgName+'); background-size: cover; background-position: 50% 50%;">' 
-							+ '<div class="content">' 
-							+ '<h6 class="category">' + data.completeMainArticleArrayList[i].tagName + '</h6><br>' 
-							+ '<h5 class="title">[완결]' + mainArticleTitle + '</h5>' 
-							+ ' <p class="description">' + mainArticleContent + '</p>' 
-							+ '<span class="writersNickName">- '+data.completeMainArticleArrayList[i].memberVO.memberNickName+' -</span>'
-							+ '<input type="hidden" name="">'
-							+ '<input type="hidden" class="mainArticleTitleNO" value="'+ data.completeMainArticleArrayList[i].mainArticleNo +'">'
-							+ '<div class="actions">' 
-							+ '<button class="btn btn-round btn-fill btn-neutral btn-modern" data-toggle="modal" data-target="#cardDetailView">Read Article</button>' 
-							+ '</div>' 
-							+ '<div class="social-line social-line-visible" data-buttons="4">' 
-							+ '<button class="btn btn-social btn-pinterest">완결된<br>잇자!</button>' 
-							+  mainLikeItHTML
-							+ pickMainArticleHTML  
-							+ '<button class="btn btn-social btn-facebook"><i class="fa fa-facebook-official"></i><br>공유하자!</button>' 
-							+ '</div>  <!-- end social-line social-line-visible --></div></div> <!-- end card --></div><!-- card-box col-md-4 -->'
+							'<article class="white-panel">'
+							+'<h4><a href="#">' + data.completeMainArticleArrayList[i].mainArticleTitle + '</a></h4>'
+							+'<h6 class="category">' + data.completeMainArticleArrayList[i].tagName + '</h6>'
+							+'<img src="resources/uploadImg/articleBg/'+data.completeMainArticleArrayList[i].mainArticleImgVO.mainArticleImgName+'" alt="">'
+							+'<div class="social-line social-line-visible" data-buttons="4">'
+							+'<button class="btn btn-social btn-pinterest">'
+							+'<span class="time_area">완결된<br>잇자!</span>'
+							+'</button>'
+							+mainLikeItHTML
+							+pickMainArticleHTML
+							+'<button class="btn btn-social btn-facebook">'
+							+'<i class="fa fa-facebook-official"></i><br> 공유!'
+							+'</button>'
+							+'</div>'
+							+'<!-- end social-line social-line-visible -->'
+							+'<p class="card-content"/>'
+							+'<p class="description">' + data.completeMainArticleArrayList[i].mainArticleContent + '</p>'
+							+'<a href="mypage.neon?memberEmail=' + data.completeMainArticleArrayList[i].memberVO.memberEmail + '" style="" tabindex="1" class="btn btn-lg btn-warning myNickDetail" role="button" data-toggle="popover" title="' + data.completeMainArticleArrayList[i].memberVO.memberNickName + '님, ' + data.completeMainArticleArrayList[i].memberVO.rankingVO.memberGrade + ' PTS(' + data.completeMainArticleArrayList[i].memberVO.memberPoint + ' / ' + data.completeMainArticleArrayList[i].memberVO.rankingVO.maxPoint + ')" data-content="' + data.completeMainArticleArrayList[i].memberVO.memberNickName + '님 Click하여 페이지 보기" >'
+							+'<span class="writersNickName">- ' + data.completeMainArticleArrayList[i].memberVO.memberNickName + ' -</span></a>'
+							+'</article>'
 					}
 					}else{
 						infinityScrollTestSource +=
-							'<div><hr><h4>마지막 주제글입니다!</h4>'
-							+ '<input type="hidden" id="articleEnd" value="end"><hr></div>'
+							'<hr><h4>마지막 주제글입니다!</h4>'
+							+ '<input type="hidden" id="articleEnd" value="end"><hr>'
 					}
 					$('.ajaxLoader').fadeOut(300);
-					$('.completeItjaList').append(infinityScrollTestSource);
+					$('#pinBoot').append(infinityScrollTestSource);
 				},
 			});
 		}else if($("#articleType").val()=='mainArticle'){
 			var cardBox=$(".white-panel").length;
 			var pageNo=Math.ceil((cardBox/9)+1);
+			//정렬방식을 담는다. -대협-
+			var orderByComp = $("#orderBy").val();
 			var tagName = $("#tagName").val();
 				$.ajax({
 					type:"post",
-					url:"getNewMainArticle.neon?pageNo="+pageNo+"&tagName="+tagName,
+					url:"getNewMainArticle.neon?pageNo="+pageNo+"&tagName="+tagName+"&orderBy="+orderByComp,
 					dataType:"json",
 					success:function(data){
 						if(data.newMainArticleArrayList.length!=0){
@@ -296,7 +288,6 @@ $(document).ready(function(){ //DOM이 준비되고
 							//추가될 카드 html문
 							infinityScrollTestSource +=
 								'<article class="white-panel">'
-								+'<input type="hidden" id="articleType" value="mainArticle">'
 								+'<h4><a href="#">' + data.newMainArticleArrayList[i].mainArticleTitle + '</a></h4>'
 								+'<h6 class="category">' + data.newMainArticleArrayList[i].tagName + '</h6>'
 								+'<img src="resources/uploadImg/articleBg/'+data.newMainArticleArrayList[i].mainArticleImgVO.mainArticleImgName+'" alt="">'
@@ -354,18 +345,6 @@ $(document).ready(function(){ //DOM이 준비되고
 					infinityScrollTestSource += 
 						'<input type="hidden" id="orderBy" value="tag"><input type="hidden" id="tagName" value="'+tagName.text().substring(1)+'">';
 					for(var i=0; i<data.completeMainArticleArrayList.length; i++){
-						//타이틀 길이제한 조건문
-						if(data.completeMainArticleArrayList[i].mainArticleTitle.length>12){
-							mainArticleTitle = data.completeMainArticleArrayList[i].mainArticleTitle.substring(0,12) + " ...";
-						}else{
-							mainArticleTitle = data.completeMainArticleArrayList[i].mainArticleTitle;
-						}
-						//내용 길이제한 조건문
-						if(data.completeMainArticleArrayList[i].mainArticleContent.length>18){
-							mainArticleContent = data.completeMainArticleArrayList[i].mainArticleContent.substring(0,15) + " ...";
-						}else{
-							mainArticleContent = data.completeMainArticleArrayList[i].mainArticleContent;
-						}
 						//잇자버튼을 위한 조건문
 						var mainLikeItHTML="";
 						  if(data.itjaMemberList!=null){
@@ -436,26 +415,29 @@ $(document).ready(function(){ //DOM이 준비되고
 	             			}
 							//추가될 카드 html문
 							infinityScrollTestSource +=
-								'<div class="card-box col-md-4" name="completeCardBox">' 
-								+ '<div class="card card-with-border" data-background="image" style="background-image: url(resources/uploadImg/articleBg/'+data.completeMainArticleArrayList[i].mainArticleImgVO.mainArticleImgName+'); background-size: cover; background-position: 50% 50%;">' 
-								+ '<div class="content">' 
-								+ '<h6 class="category">' + data.completeMainArticleArrayList[i].tagName + '</h6><br>' 
-								+ '<h5 class="title">[완결]' + mainArticleTitle + '</h5>' 
-								+ ' <p class="description">' + mainArticleContent + '</p>' 
-								+ '<span class="writersNickName">- '+data.completeMainArticleArrayList[i].memberVO.memberNickName+' -</span>'
-								+ '<input type="hidden" class="mainArticleTitleNO" value="'+ data.completeMainArticleArrayList[i].mainArticleNo +'">'
-								+ '<div class="actions">'
-								+ '<button class="btn btn-round btn-fill btn-neutral btn-modern" data-toggle="modal" data-target="#cardDetailView">Read Article</button>' 
-								+ '</div>' 
-								+ '<div class="social-line social-line-visible" data-buttons="4">' 
-								+ '<button class="btn btn-social btn-pinterest">완결된<br>잇자!</button>' 
-								+ mainLikeItHTML
-								+ pickMainArticleHTML 
-								+ '<button class="btn btn-social btn-facebook"><i class="fa fa-facebook-official"></i><br>공유하자!</button>' 
-								+ '</div>  <!-- end social-line social-line-visible --></div></div> <!-- end card --></div><!-- card-box col-md-4 -->'
+								'<article class="white-panel">'
+								+'<h4><a href="#">' + data.completeMainArticleArrayList[i].mainArticleTitle + '</a></h4>'
+								+'<h6 class="category">' + data.completeMainArticleArrayList[i].tagName + '</h6>'
+								+'<img src="resources/uploadImg/articleBg/'+data.completeMainArticleArrayList[i].mainArticleImgVO.mainArticleImgName+'" alt="">'
+								+'<div class="social-line social-line-visible" data-buttons="4">'
+								+'<button class="btn btn-social btn-pinterest">'
+								+'<span class="time_area">완결된<br>잇자!</span>'
+								+'</button>'
+								+mainLikeItHTML
+								+pickMainArticleHTML
+								+'<button class="btn btn-social btn-facebook">'
+								+'<i class="fa fa-facebook-official"></i><br> 공유!'
+								+'</button>'
+								+'</div>'
+								+'<!-- end social-line social-line-visible -->'
+								+'<p class="card-content"/>'
+								+'<p class="description">' + data.completeMainArticleArrayList[i].mainArticleContent + '</p>'
+								+'<a href="mypage.neon?memberEmail=' + data.completeMainArticleArrayList[i].memberVO.memberEmail + '" style="" tabindex="1" class="btn btn-lg btn-warning myNickDetail" role="button" data-toggle="popover" title="' + data.completeMainArticleArrayList[i].memberVO.memberNickName + '님, ' + data.completeMainArticleArrayList[i].memberVO.rankingVO.memberGrade + ' PTS(' + data.completeMainArticleArrayList[i].memberVO.memberPoint + ' / ' + data.completeMainArticleArrayList[i].memberVO.rankingVO.maxPoint + ')" data-content="' + data.completeMainArticleArrayList[i].memberVO.memberNickName + '님 Click하여 페이지 보기" >'
+								+'<span class="writersNickName">- ' + data.completeMainArticleArrayList[i].memberVO.memberNickName + ' -</span></a>'
+								+'</article>'
 					}
 					$('.ajaxLoader').fadeOut(300);
-					$('.completeItjaList').html(infinityScrollTestSource);
+					$('#pinBoot').html(infinityScrollTestSource);
 				}
 			});
 		}else if($("#articleType").val()=="mainArticle"){
@@ -467,18 +449,6 @@ $(document).ready(function(){ //DOM이 준비되고
 					infinityScrollTestSource += 
 						'<input type="hidden" id="orderBy" value="tag"><input type="hidden" id="tagName" value="'+tagName.text().substring(1)+'">';
 					for(var i=0; i<data.newMainArticleArrayList.length; i++){
-						//타이틀 길이제한 조건문
-						if(data.newMainArticleArrayList[i].mainArticleTitle.length>12){
-							mainArticleTitle = data.newMainArticleArrayList[i].mainArticleTitle.substring(0,12) + " ...";
-						}else{
-							mainArticleTitle = data.newMainArticleArrayList[i].mainArticleTitle;
-						}
-						//내용 길이제한 조건문
-						if(data.newMainArticleArrayList[i].mainArticleContent.length>18){
-							mainArticleContent = data.newMainArticleArrayList[i].mainArticleContent.substring(0,15) + " ...";
-						}else{
-							mainArticleContent = data.newMainArticleArrayList[i].mainArticleContent;
-						}
 						//잇자버튼을 위한 조건문
 						var mainLikeItHTML="";
 						  if(data.itjaMemberList!=null){
@@ -552,26 +522,29 @@ $(document).ready(function(){ //DOM이 준비되고
 							
 							//추가될 카드 html문
 							infinityScrollTestSource +=
-								'<div class="card-box col-md-4" name="newCardBox">' 
-								+ '<div class="card card-with-border" data-background="image" style="background-image: url(resources/uploadImg/articleBg/'+data.newMainArticleArrayList[i].mainArticleImgVO.mainArticleImgName+'); background-size: cover; background-position: 50% 50%;">' 
-								+ '<div class="content">' 
-								+ '<h6 class="category">' + data.newMainArticleArrayList[i].tagName + '</h6><br>' 
-								+ '<h5 class="title">' + mainArticleTitle + '</h5>' 
-								+ ' <p class="description">' + mainArticleContent + '</p>' 
-								+ '<span class="writersNickName">- '+data.newMainArticleArrayList[i].memberVO.memberNickName+' -</span>'
-								+ '<input type="hidden" class="mainArticleTitleNO" value="'+ data.newMainArticleArrayList[i].mainArticleNo +'">'
-								+ '<div class="actions">'
-								+ '<button class="btn btn-round btn-fill btn-neutral btn-modern" data-toggle="modal" data-target="#cardDetailView">Read Article</button>' 
-								+ '</div>' 
-								+ '<div class="social-line social-line-visible" data-buttons="4">' 
-								+ '<button class="btn btn-social btn-pinterest">새로운<br>잇자!</button>' 
-								+ mainLikeItHTML
-								+ pickMainArticleHTML 
-								+ '<button class="btn btn-social btn-facebook"><i class="fa fa-facebook-official"></i><br>공유하자!</button>' 
-								+ '</div>  <!-- end social-line social-line-visible --></div></div> <!-- end card --></div><!-- card-box col-md-4 -->'
+								'<article class="white-panel">'
+								+'<h4><a href="#">' + data.newMainArticleArrayList[i].mainArticleTitle + '</a></h4>'
+								+'<h6 class="category">' + data.newMainArticleArrayList[i].tagName + '</h6>'
+								+'<img src="resources/uploadImg/articleBg/'+data.newMainArticleArrayList[i].mainArticleImgVO.mainArticleImgName+'" alt="">'
+								+'<div class="social-line social-line-visible" data-buttons="4">'
+								+'<button class="btn btn-social btn-pinterest">'
+								+'<span class="time_area">새로운<br>잇자!</span>'
+								+'</button>'
+								+mainLikeItHTML
+								+pickMainArticleHTML
+								+'<button class="btn btn-social btn-facebook">'
+								+'<i class="fa fa-facebook-official"></i><br> 공유!'
+								+'</button>'
+								+'</div>'
+								+'<!-- end social-line social-line-visible -->'
+								+'<p class="card-content"/>'
+								+'<p class="description">' + data.newMainArticleArrayList[i].mainArticleContent + '</p>'
+								+'<a href="mypage.neon?memberEmail=' + data.newMainArticleArrayList[i].memberVO.memberEmail + '" style="" tabindex="1" class="btn btn-lg btn-warning myNickDetail" role="button" data-toggle="popover" title="' + data.newMainArticleArrayList[i].memberVO.memberNickName + '님, ' + data.newMainArticleArrayList[i].memberVO.rankingVO.memberGrade + ' PTS(' + data.newMainArticleArrayList[i].memberVO.memberPoint + ' / ' + data.newMainArticleArrayList[i].memberVO.rankingVO.maxPoint + ')" data-content="' + data.newMainArticleArrayList[i].memberVO.memberNickName + '님 Click하여 페이지 보기" >'
+								+'<span class="writersNickName">- ' + data.newMainArticleArrayList[i].memberVO.memberNickName + ' -</span></a>'
+								+'</article>'
 					}
 					$('.ajaxLoader').fadeOut(300);
-					$('.newItjaList').html(infinityScrollTestSource);
+					$('#pinBoot').html(infinityScrollTestSource);
 				}
 			});
 		}
